a87df7998de14d53b7167d7ee7e1f8c1a99f09ac
Sebastien Mairre <mairre.sebastien@gmail.com>
2022-05-02 21:46:30 +0200

    steps 1-2-3 done, add assets folder and normalize.css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,38 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+// Helper function that sets many attributes at once
+function setAttributes(el, attrs) {
+    for(let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+    }
+}
 
-    const picture = `assets/photographers/${portrait}`;
+
+// Generates photographers cards on index.html with json data
+function photographerFactory(data) {
+    const { name, id, city, country, tagline, price, portrait } = data;
+
+    const picture = `assets/FishEye_Photos/Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        setAttributes(article, {"id": id, "aria-label": `Link to ${name}'s page`, "tabindex": "0"});
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        setAttributes(img, {"src": picture, "alt": `profile picture of ${name}`, "tabindex": "0"})
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3 = document.createElement( 'h3');
+        h3.textContent = `${city}, ${country}`;
+        const citeTagLine = document.createElement( 'cite' );
+        citeTagLine.textContent = tagline;
+        const paraPrice = document.createElement( 'p' );
+        paraPrice.textContent = `${price}€/jour`;
+        const anchor = document.createElement( 'a' );
+        setAttributes(anchor, {"href": ` `, "aria-label": `${name}`});
+        article.appendChild(anchor);
+        anchor.appendChild(img);
+        anchor.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(citeTagLine);
+        article.appendChild(paraPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
